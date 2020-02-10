144d9e40c837aea9de899fdee5305ea734d2d6db
Chris Jung <cjung@bu.edu>
2020-02-09 23:24:37 -0500

    added final code and test

---
+++ apitest.py
@@ -0,0 +1,12 @@
+import openweather
+
+def testing():
+    airport_weather("Total Rf Heliport")
+    airport_weather("Washington Dulles International Airport")
+    airport_weather("Idaho Falls Regional Airport")
+    airport_weather("Jamestown Regional Airport")
+    airport_weather("Rush County Airport")
+    airport_weather("Mooreland Municipal Airport")
+    airport_weather("Icheon-dong Heliport")
+    airport_weather("Louis Armstrong New Orleans International Airport")
+            
---
+++ openweather.py
@@ -0,0 +1,29 @@
+import requests
+import csv
+
+api_key = 'd03a56cf2821428e5ad66c8317e604d1'
+                
+def airport_weather(airportname):
+    with open ('airports.csv') as airports:
+        listofairports = csv.DictReader(airports)
+        #listofairports = csv.DictReader('airports.csv')
+        for row in listofairports:
+            if row['name'] == airportname:
+                long = row['longitude_deg']
+                lat = row['latitude_deg']
+                url = "http://api.openweathermap.org/data/2.5/weather?" + "lat=" + lat + "&lon=" + long + "&appid=" + api_key
+                data = requests.get(url).json()
+
+                print("Weather: ", data['weather'][0]['description'])
+                print("Temperature: ", data['main']['temp'])
+                print("Wind speed: ", data['wind']['speed'])
+                print("Humidity: ", data['main']['humidity'])
+                print("Clouds: ", data['clouds']['all'])
+                print("Long:", data['coord']['lon'], "Lat:",data['coord']['lat'])
+                print("Humidity:", data['main']['humidity'])
+                
+
+airportname = input("Please enter the name of an airport: ")
+airport_weather(airportname)
+ 
+
