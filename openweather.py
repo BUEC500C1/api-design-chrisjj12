73ebee47bda26af49e0535dd161574c590ace4c0
Chris Jung <cjung@bu.edu>
2020-05-09 22:10:31 -0400

    changes made

--- openweather.py
+++ openweather.py
@@ -1,7 +1,7 @@
 import requests
 import csv
 
-api_key = 'd03a56cf2821428e5ad66c8317e604d1'
+api_key = 'c3ac39f20eed9f70d7bd7df5aba9850d'
                 
 def airport_weather(airportname):
     
@@ -11,22 +11,20 @@ def airport_weather(airportname):
 
                 for row in listofairports:
                     if row['name'] == airportname:
-                        long = row['longitude_deg']
+                        lon = row['longitude_deg']
                         lat = row['latitude_deg']
-                        url = "http://api.openweathermap.org/data/2.5/weather?" + "lat=" + lat + "&lon=" + long + "&appid=" + api_key
+                        url = "http://api.openweathermap.org/data/2.5/weather?" + "lat=" + lat + "&lon=" + lon + "&appid=" + api_key
                         data = requests.get(url).json()
 
-                        weather_stats= []
-
-                        weather_stats.append("Weather: " + data['weather'][0]['description'])
-                        weather_stats.append("Temperature: " + data['main']['temp'])
-                        weather_stats.append("Wind speed: " +  data['wind']['speed'])
-                        weather_stats.append("Humidity: " +  data['main']['humidity'])
-                        weather_stats.append("Clouds: " +  data['clouds']['all'])
-                        weather_stats.append("Long: " +  data['coord']['lon'] +  "Lat: " + data['coord']['lat'])
-                        weather_stats.append("Humidity: " +  data['main']['humidity'])
-
+                        weather_stats = []
+                        weather_stats.append("Weather: " + str(data['weather'][0]['description']))
+                        weather_stats.append("Temperature: " + str(data['main']['temp']))
+                        weather_stats.append("Wind speed: " +  str(data['wind']['speed']))
+                        weather_stats.append("Humidity: " +  str(data['main']['humidity']))
+                        weather_stats.append("Clouds: " +  str(data['clouds']['all']))
+                        weather_stats.append("Long: " +  str(data['coord']['lon']) +  "Lat: " + str(data['coord']['lat']))
                         return weather_stats
             except:
                 print("Error")
                 return []
+
